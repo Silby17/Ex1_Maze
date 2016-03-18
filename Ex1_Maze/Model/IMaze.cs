c19593e6929ed2ca330fa152bf56d122644ea866
Yossi Silberhaft <yossisilb@gmail.com>
2016-03-18 15:23:02 +0200

    Added Maze interface and class

---
+++ Ex1_Maze/Model/IMaze.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1_Maze.Model
+{
+    interface IMaze
+    {
+        void solveMaze(string mazeName);
+        void getSolution();
+    }
+}
