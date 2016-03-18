cf87c3727814ceafb9bbdddc1bb1f4bcf05d2d68
Yossi Silberhaft <yossisilb@gmail.com>
2016-03-18 15:14:34 +0200

    Initial Commit

---
+++ Ex1_Maze/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1_Maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Program");
+        }
+    }
+}
