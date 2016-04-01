235f2f672399acfae5b3b50b48fcffef36dfb4c1
Yossi Silberhaft <yossisilb@gmail.com>
2016-04-01 14:03:16 +0300

    Created a new class that will run the Client

---
+++ Client/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class Client
+    {
+        private int PORT;
+        private IPAddress IP;
+
+        /// <summary>
+        /// Starts the running of the Client/// </summary>
+        public void StartClient()
+        {
+            Console.WriteLine("Client Started");
+            PORT = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["Port"]);
+            IP = IPAddress.Parse(System.Configuration.ConfigurationManager.AppSettings["IP"]);
+
+            IPEndPoint ipep = new IPEndPoint(IP, PORT);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                server.Connect(ipep);
+                Console.WriteLine("Connected to Server");
+                byte[] data = new byte[1024];
+                int recv = server.Receive(data);
+                string stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine(stringData);
+
+                while (true)
+                {
+                    Console.WriteLine("Enter to send to Server:");
+                    string input = Console.ReadLine();
+                    if (input == "exit") break;
+                    server.Send(Encoding.ASCII.GetBytes(input));
+                    stringData = Encoding.ASCII.GetString(data, 0, recv);
+
+                    recv = server.Receive(data);
+                    stringData = Encoding.ASCII.GetString(data, 0, recv);
+                    Console.WriteLine(stringData);
+                }
+                server.Shutdown(SocketShutdown.Both);
+                server.Close();
+            }
+            catch (SocketException e) { Console.WriteLine("Unable to connect to server." + e.ToString()); }
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,53 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Client
 {
     class Program
     {
-        public static int PORT;
-        public static IPAddress IP;
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Client Started");
-            PORT = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["Port"]);
-            IP = IPAddress.Parse(System.Configuration.ConfigurationManager.AppSettings["IP"]);
-
-            IPEndPoint ipep = new IPEndPoint(IP, PORT);
-            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            try
-            {
-                server.Connect(ipep);
-                Console.WriteLine("Connected to Server");
-                byte[] data = new byte[1024];
-                int recv = server.Receive(data);
-                string stringData = Encoding.ASCII.GetString(data, 0, recv);
-                Console.WriteLine(stringData);
-
-                while (true)
-                {
-                    Console.WriteLine("Enter to send to Server:");
-                    string input = Console.ReadLine();
-                    if (input == "exit") break;
-                    server.Send(Encoding.ASCII.GetBytes(input));
-                    stringData = Encoding.ASCII.GetString(data, 0, recv);
-
-                    recv = server.Receive(data);
-                    stringData = Encoding.ASCII.GetString(data, 0, recv);
-                    Console.WriteLine(stringData);
-                }
-                server.Shutdown(SocketShutdown.Both);
-                server.Close();
-            }
-            catch (SocketException e) { Console.WriteLine("Unable to connect to server." + e.ToString()); }
+            Client client = new Client();
+            client.StartClient();          
         }
     }
 }
