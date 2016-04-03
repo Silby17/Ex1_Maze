e0b62b3486c7059f5de7d2a4167ef12480ec23c0
Yossi Silberhaft <yossisilb@gmail.com>
2016-04-03 19:51:58 +0300

    Added Event and Delegate to the Interface

--- Server2/Server2/ICommandable.cs
+++ Server2/Server2/ICommandable.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 
 namespace Server2
 {
+    public delegate void ExecutionDone(Object source, EventArgs e);
+
     public interface ICommandable
     {
+        event ExecutionDone execDone;
         void Execute(List<object> args);
+        void PublishEvent();
     }
 }
