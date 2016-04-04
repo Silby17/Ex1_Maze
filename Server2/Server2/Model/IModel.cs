53c85438ca2e057f1e4b6de8b7e0eb2665b4daf0
Yossi Silberhaft <yossisilb@gmail.com>
2016-04-04 23:12:40 +0300

    FINAL COMMIT

---
+++ Server2/Server2/Model/IModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server2
+{
+    public delegate void NewModelChange(Object source, EventArgs e);
+
+    public interface IModel
+    {
+        event NewModelChange newModelChange;
+        void ExecuteCommandalbe(List<object> list);
+        void CreateOptionsDictionary();
+        void PublishEvent();
+        string GetModelChange();
+    }
+}
