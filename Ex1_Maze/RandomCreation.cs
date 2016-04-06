0306c286db7c9d962024b5f0bf018623468ddf62
Yossi Silberhaft <yossisilb@gmail.com>
2016-04-06 14:30:41 +0300

    Done and Working

--- Ex1_Maze/RandomCreation.cs
+++ Ex1_Maze/RandomCreation.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ex1_Maze
 {
@@ -11,24 +9,44 @@ namespace Ex1_Maze
         private int height;
         private int width;
         private GeneralMaze<T> maze;
+        private Node<T> last;
+        private Random rand;
 
-        //By prim algo
+        /// <summary>
+        /// Main Method that will created a maze using Prims Algorithm </summary>
+        /// <param name="createable">Maze</param>
         public void create(ICreateable<T> createable)
         {
-            maze = createable.GetMaze();
-            height = createable.GetHeight();
-            width = createable.GetWidth();
-            createable.GetMaze().Print();
+            this.maze = createable.GetMaze();
+            this.height = createable.GetHeight();
+            this.width = createable.GetWidth();
             List<Node<T>> primList = new List<Node<T>>();
-            primList.Add(createable.GetStartPoint());
+            rand = new Random();
+            
+            //Sets a random Starting Point
+            int rRow = rand.Next(height);
+            int rCol = rand.Next(width);
+            maze.GetNode(rRow, rCol).SetValue(0);
+            maze.SetStartPoints(rRow, rCol);
 
+            //Adds Node to the Prims List
+            primList.Add(maze.GetNode(rRow, rCol));
+            
+            //Will run as long as the List is not empty
             while (primList.Count > 0)
             {
                 primList = primList.Distinct().ToList();
                 this.getNeighbors(primList);
             }
+            //Sets the end Point of the maze
+            this.maze.SetEndPoints(last.GetRow(), last.GetCol());
         }
 
+
+        /// <summary>
+        /// This will get all the neighbours of a certain Node</summary>
+        /// <param name="neighbors">Returns the list of neighbours</param>
+        /// <returns></returns>
         public List<Node<T>> getNeighbors(List<Node<T>> neighbors)
         {
             int rundomNumber = new Random().Next(neighbors.Count);
@@ -45,43 +63,40 @@ namespace Ex1_Maze
                 {
                     colBetwen *= -1;
                 }
-
                 if (rowBetwen < 0)
                 {
                     rowBetwen *= -1;
                 }
-            
                 this.maze.SetCell(rowBetwen, colBetwen, 0);
             }
 
-            //remove from list
-            neighbors.Remove(current);
 
+            //Removes the current Node from the List
+            neighbors.Remove(current);
+            this.last = current;
+            //Saves the indecies of the current node
             int col = current.GetCol();
             int row = current.GetRow();
            
-            //up
+            //Checks Up
             if (row - 2 >= 0 && this.maze.GetValue(row - 1, col) == 1 &&
                 this.maze.GetValue(row - 2, col) == 1)
             {
                 neighbors.Add(new Node<T>(row - 2, col, current));
             }
-
-            //down
+            //Checks Down
             if (row + 2 < this.height && this.maze.GetValue(row + 1, col) == 1 &&
                 this.maze.GetValue(row + 2, col) == 1)
             {
                 neighbors.Add(new Node<T>(row + 2, col, current));
             }
-
-            //left
+            //Checks Left
             if (col - 2 >= 0 && this.maze.GetValue(row, col - 1) == 1 &&
                 this.maze.GetValue(row, col - 2) == 1)
             {
-                neighbors.Add(new Node<T>(row, col - 2, current));
+                neighbors.Add(new Node<T>(row, col - 1, current));
             }
-
-            //right
+            //Checks Right
             if (col + 2 < this.width && this.maze.GetValue(row, col + 1) == 1 &&
                this.maze.GetValue(row, col + 2) == 1)
             { 
@@ -89,7 +104,5 @@ namespace Ex1_Maze
             }
             return neighbors;
         }
-
     }
 }
-    
