83d0850c550b7c64fd68db84c13d7453de83d34c
Yossi Silberhaft <yossisilb@gmail.com>
2016-04-06 14:18:11 +0300

    Moved certain methods up to IMaze and removed Double methods

--- Ex1_Maze/ICreateable.cs
+++ Ex1_Maze/ICreateable.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Ex1_Maze
 {
     public interface ICreateable<T>
     {
+        GeneralMaze<T> GetMaze();
         int GetHeight();
         int GetWidth();
-        GeneralMaze<T> GetMaze();
-        Node<T> GetStartPoint();
-        Node<T> GetEndPoint();
     }
 }
