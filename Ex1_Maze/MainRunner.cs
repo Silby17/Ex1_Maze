539b49c93b6b46795a66c70058c3a740af095959
Yossi Silberhaft <yossisilb@gmail.com>
2016-04-06 14:29:33 +0300

    Played around with for testing

--- Ex1_Maze/MainRunner.cs
+++ Ex1_Maze/MainRunner.cs
@@ -11,8 +11,7 @@ namespace Ex1_Maze
 
         public void startProgram()
         {
-            _2DMaze<int> maze = new _2DMaze<int>(5, 5);
-            
+            _2DMaze<int> maze = new _2DMaze<int>(5, 5);            
             GeneralMaze<int> Cm = new GeneralMaze<int>(maze);
             Cm.Generate("mazushshsh", 0);
            // Cm.Solve("nava", 1);
