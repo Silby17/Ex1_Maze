bc6462d93f792128a97112b01a78161923c61f93
Yossi Silberhaft <yossisilb@gmail.com>
2016-04-12 18:15:41 +0300

    Solve Comments Done and all working.

--- Server2/Server2/Options/Solve.cs
+++ Server2/Server2/Options/Solve.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using System.Web.Script.Serialization;
 using System.Collections.Generic;
-using System.Linq;
+using Newtonsoft.Json.Linq;
 using System.Net.Sockets;
+using System.Linq;
+using Ex1_Maze;
+using System;
 
 
 namespace Server2.Options
@@ -10,27 +13,31 @@ namespace Server2.Options
     {
         public event ExecutionDone execDone;
         private Socket clientToReturnTo;
+        private string JSONMaze;
 
 
         /// <summary>
-        /// This Method will Execute the Solving Algorithm</summary>
-        /// <param name="args">The info of what maze and what Algorithm</param>
-        /// <returns></returns>
-        public string Execute(List<object> args)
-        {
-            List<string> strParams = args.Select(s => (string)s).ToList();
-            string mazeName = strParams[1];
-            int type = Int32.Parse(strParams[2]);
-
-            return "DONE";
-        }
-
-        public void Execute(List<object> args, Socket client)
+        /// Constructor Method that will get a list or arguments,
+        /// the client that send the request and the
+        /// List of Mazes if needed</summary>
+        /// <param name="args">List of arguments from client</param>
+        /// <param name="client">Socket of the sender</param>
+        /// <param name="mazeList">List of mazes</param>
+        public void Execute(List<object> args, Socket client, 
+            Dictionary<string, GeneralMaze<int>> mazeList)
         {
             this.clientToReturnTo = client;
             List<string> strParams = args.Select(s => (string)s).ToList();
             string mazeName = strParams[1];
             int type = Int32.Parse(strParams[2]);
+            if (mazeList.ContainsKey(mazeName))
+            {
+                mazeList[mazeName].Solve(type);
+                JavaScriptSerializer ser = new JavaScriptSerializer();
+                string jsonMaze = ser.Serialize(mazeList[mazeName]);
+                this.JSONMaze = JToken.Parse(jsonMaze).ToString();
+                PublishEvent();
+            }            
         }
 
 
@@ -40,11 +47,19 @@ namespace Server2.Options
         {
             if(execDone != null){ execDone(this, EventArgs.Empty);}
         }
+
+
         /// <summary>
         /// Returns the Client socket that send the request</summary>
         /// <returns>Clients Socket Details</returns>
         public Socket GetClientSocket()
         { return this.clientToReturnTo; }
 
+
+        /// <summary>
+        /// This will return the JSON format of the Solved maze</summary>
+        /// <returns>JSON string</returns>
+        public string GetJSON()
+        { return this.JSONMaze; }
     }
 }
