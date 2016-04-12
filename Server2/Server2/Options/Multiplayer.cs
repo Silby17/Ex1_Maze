cae453a68c5869f9dd2bcd1bb1aeaf9d2a215f37
Yossi Silberhaft <yossisilb@gmail.com>
2016-04-13 00:22:41 +0300

    Added a lot, creating new game and Joining existing. Joining is still not complete

--- Server2/Server2/Options/Multiplayer.cs
+++ Server2/Server2/Options/Multiplayer.cs
@@ -5,6 +5,7 @@ using System.Net.Sockets;
 using Ex1_Maze;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace Server2.Options
 {
@@ -12,7 +13,11 @@ namespace Server2.Options
     {
         public event ExecutionDone execDone;
         private Socket clientToReturnTo;
-
+        private List<Game> gamesList;
+        private Dictionary<string, GeneralMaze<int>> mazeList;
+        private int HIEGHT, WIDTH;
+        private string JSONResult;
+        private List<Game> gameList;
 
         /// <summary>
         /// Constructor Method that will get a list or arguments, the client that 
@@ -22,9 +27,45 @@ namespace Server2.Options
         /// <param name="mazeList">List of mazes</param>
         public void Execute(List<object> args, Socket client, Dictionary<string, GeneralMaze<int>> mazeList)
         {
-            this.clientToReturnTo = client;
-            List<string> strParams = args.Select(s => (string)s).ToList();
-            string gameName = strParams[1];
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            this.mazeList = mazeList;
+            SetSize();           
+            
+            //Creates a new Player
+            Player player = new Player(client);
+            //Gets the name of the Game
+            string gameName = (string)args[1];
+            //Converts the List of games from type object
+            List<Game> games = (List<Game>)args[2];
+
+            //If the Games List is not empty
+            if(games.Count != 0)
+            {
+                foreach(Game g in games)
+                {
+                    if(g.GetGameName() == gameName)
+                    {
+                        //The Game already Exists in the list of games
+
+                        //Adds new player to the game
+                        g.AssignPlayerToGame(player);
+                        g.CreateSecondMaze();
+
+                    }
+
+                }
+            }
+            //Create a new Game if the game doesnt Exist
+            else
+            {
+                GeneralMaze<int> newMaze = CreateMultiMaze(gameName);
+                this.mazeList.Add(newMaze.Name, newMaze);
+                Game newGame = new Game(newMaze, gameName);
+                newGame.AddPlayers(player);
+                games.Add(newGame);
+                string msg = "First Player To join";
+                this.JSONResult = ser.Serialize(msg);
+            }
         }
 
 
@@ -41,5 +82,27 @@ namespace Server2.Options
         /// <returns>Clients Socket Details</returns>
         public Socket GetClientSocket()
         { return this.clientToReturnTo; }
+
+
+        public GeneralMaze<int> CreateMultiMaze(string name)
+        {
+            _2DMaze<int> newMaze = new _2DMaze<int>(HIEGHT, WIDTH);
+            GeneralMaze<int> newGM = new GeneralMaze<int>(newMaze);
+            Random rand = new Random();
+            int type = rand.Next(0, 2);
+            string mazeName = name + "_1" + " " + type;
+            newGM.Generate(mazeName, type);
+
+            return newGM;
+        }
+
+        public string GetJSON()
+        { return this.JSONResult; }
+
+        public void SetSize()
+        {
+            this.HIEGHT = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["HEIGHT"]);
+            this.WIDTH = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["WIDTH"]);
+        }
     }
 }
