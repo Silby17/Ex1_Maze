91cdb0b9a0b8fcc0817bbe69a1eb5e4b38a10f57
Yossi Silberhaft <yossisilb@gmail.com>
2016-04-13 21:42:30 +0300

    Option 4 - Play is fully working. Still need to Do option 5 and FIX the JSON format of option 3

--- Ex1_Maze/Game.cs
+++ Ex1_Maze/Game.cs
@@ -10,20 +10,19 @@ namespace Ex1_Maze
 {
     public class Game
     {
-
         public List<Player> playersList;
         public List<GeneralMaze<int>> mazeList;
-        //private Dictionary<GeneralMaze<int>, Player> playersAndMazes;
         private string GameName;
         public Player player1{get; set;}
         public Player player2 { get; set; }
         private GeneralMaze<int> mazePlayer1;
         private GeneralMaze<int> mazePlayer2;
 
-        public Game()
-        { }
 
-
+        /// <summary>
+        /// Constructor method that gets a maze and the name of the Game</summary>
+        /// <param name="m">The new game Maze</param>
+        /// <param name="name">The Name of the Game</param>
         public Game(GeneralMaze<int> m, string name)
         {
             this.mazePlayer1 = m;
@@ -58,46 +57,68 @@ namespace Ex1_Maze
         }
 
 
+        /// <summary>
+        /// Returns the name of the Current Game</summary>
+        /// <returns>The name of the game</returns>
         public string GetGameName()
-        {
-            return this.GameName;
-        }
+        { return this.GameName;}
 
-        public void AddPlayers(Player p)
-        {
-            this.playersList.Add(p);
-        }
 
+        /// <summary>
+        /// Adds player to the current Game</summary>
+        /// <param name="p">Player to be added</param>
         public void AssignPlayerToGame(Player player)
-        {
-            this.playersList.Add(player);
-        }
+        { this.playersList.Add(player); }
 
+
+        /// <summary>
+        /// Method that returns the list of Players</summary>
+        /// <returns></returns>
         public List<Player> GetPlayersList()
-        {
-            return this.playersList;
-        }
+        { return this.playersList; }
 
-        public void AddPlayerAndMaze(GeneralMaze<int> maze, Player p)
-        {
-            //this.playersAndMazes.Add(maze, p);
-        }
-
-
-
+        
+        /// <summary>
+        /// Sets the maze for the second player </summary>
+        /// <param name="maze">MAze to be set</param>
         public void SetMazePlayer2(GeneralMaze<int> maze)
+        { this.mazePlayer2 = maze; }
+
+
+        /// <summary>
+        /// This method will return the player that is playing
+        /// against the current Player</summary>
+        /// <param name="current"></param>
+        /// <returns></returns>
+        public Player GetOtherPlayer(Player current)
         {
-            this.mazePlayer2 = maze;
+            if(current == player1)
+            {
+                return player2;
+            }
+            else
+            {
+                return player1;
+            }
         }
 
 
+        /// <summary>
+        /// Returns the maze of player1</summary>
+        /// <returns>Player 1 maze</returns>
         public GeneralMaze<int> GetPlayer1Maze()
         { return this.mazePlayer1; }
 
+
+        /// <summary>
+        /// Returns the Maze of the second player</summary>
+        /// <returns></returns>
         public GeneralMaze<int> GetPlayer2Maze()
         { return this.mazePlayer2; }
 
 
+        /// <summary>
+        /// Sets the players in the Game</summary>
         public void SetPlayers()
         {
             this.player1 = playersList[0];
